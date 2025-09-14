498fa43a2f843351cdc4a40a518efb24fb092f53
Seldarzu <arzuuxselda@gmail.com>
2025-09-14 19:37:09 +0300

    Organize project folders and add a few examples

---
+++ loops_and_conditions/for-loops.py
@@ -0,0 +1,5 @@
+for i in range (3):
+    print("Hello")
+
+for i in range (4):
+    print("Hello "+ str(i))
---
+++ loops_and_conditions/ranges.py
@@ -0,0 +1,21 @@
+for i in range(4):
+    print(i)#0,1,2,3
+
+print()
+
+
+for i in range(1,4):
+    print(i)#1,2,3
+
+
+print()
+
+for i in range(4,11,2):
+    print(i)#4,6,8,10
+
+print()
+
+
+for hello in range(4,11,2):
+    print(hello)#4,6,8,10 we can change variable name 
+
